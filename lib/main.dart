59b54cb2dfa160b116b9a6cee770470dd6890c94
Andrei L <andrei.leca55@gmail.com>
2023-01-14 13:07:20 +0100

    Update code

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  static String baseUrl = "https://dev-panasonic.pantheonsite.io/";
+  static String baseUrl = "https://dev-dominewptest.pantheonsite.io";
   static Client client = Client();
   static ProductsApi productsApi = ProductsApi(client, baseUrl);
 
